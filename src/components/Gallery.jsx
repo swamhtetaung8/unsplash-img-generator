77ce67064e28e8d70768f5e8a785c0046d7287d6
Swam Htet Aung <swamhtetaungg@gmail.com>
2023-07-11 21:12:01 +0630

    Add empty state when no image is found

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -21,35 +21,43 @@ const Gallery = () => {
     );
   }
 
-  return (
-    <div className="grid grid-cols-1 gap-10 m-10 lg:grid-cols-3 md:grid-cols-2">
-      {images.map((image) => (
-        <article key={image.id} className="group">
-          <img
-            alt="Lava"
-            src={image.urls.small}
-            className="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
-          />
+  if (images.length > 0) {
+    return (
+      <div className="grid grid-cols-1 gap-10 m-10 lg:grid-cols-3 md:grid-cols-2">
+        {images.map((image) => (
+          <article key={image.id} className="group">
+            <img
+              alt="Lava"
+              src={image.urls.small}
+              className="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+            />
 
-          <div className="p-4">
-            <h3 className="font-medium text-gray-900 truncate text-md first-letter:uppercase">
-              {image.alt_description}
-            </h3>
-            <div className="flex items-center justify-between mt-3">
-              <p className="text-sm text-black/50 md:text-md">
-                By {image.user.name}
-              </p>
+            <div className="p-4">
+              <h3 className="font-medium text-gray-900 truncate text-md first-letter:uppercase">
+                {image.alt_description}
+              </h3>
+              <div className="flex items-center justify-between mt-3">
+                <p className="text-sm text-black/50 md:text-md">
+                  By {image.user.name}
+                </p>
 
-              <a
-                className="inline-block px-5 py-2 text-sm font-medium text-indigo-600 border border-indigo-600 rounded md:px-10 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
-                href={image.urls.full}
-                target="blank">
-                View
-              </a>
+                <a
+                  className="inline-block px-5 py-2 text-sm font-medium text-indigo-600 border border-indigo-600 rounded md:px-10 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+                  href={image.urls.full}
+                  target="blank">
+                  View
+                </a>
+              </div>
             </div>
-          </div>
-        </article>
-      ))}
+          </article>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen mt-20 text-3xl text-center md:text-5xl">
+      No image found.
     </div>
   );
 };
